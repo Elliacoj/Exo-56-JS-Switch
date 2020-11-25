6300a3b4a0a6a38206c410ab74e5c59c229431bc
Elliacoj <73291249+Elliacoj@users.noreply.github.com>
2020-11-25 14:00:28 +0100

    Modif

---
+++ script.js
@@ -0,0 +1,24 @@
+let reponse = prompt("Vous utilisez quel matériel?");
+
+switch (reponse) {
+    case "souris":
+        alert("Vous utilisez une " + reponse);
+        break;
+    case "clavier":
+        alert("Vous utilisez une " + reponse);
+        break;
+    case "ordinateur":
+        alert("Vous utilisez une " + reponse);
+        break;
+    case "tapis":
+        alert("Vous utilisez une " + reponse);
+        break;
+    case "imprimante":
+        alert("Vous utilisez une " + reponse);
+        break;
+    case "ondulateur":
+        alert("Vous utilisez une " + reponse);
+        break;
+    default:
+        alert("L'accessoire est inconnu")
+}
